225cfbc0ef61ef295eb9494e850d98a9584d0494
imcoddy <imcoddy@gmail.com>
2015-04-22 23:30:54 +0900

    add soluton for path-sum

---
+++ path-sum.js
@@ -0,0 +1,42 @@
+/**
+Path Sum
+Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+
+For example:
+Given the below binary tree and sum = 22,
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \      \
+        7    2      1
+return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+
+Hide Tags Tree Depth-first Search
+*/
+
+/**
+ * Definition for binary tree
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * This one is a bit tricky, as it includes test case like ({}, 0)
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @returns {boolean}
+ */
+var hasPathSum = function(root, sum) {
+    if (root) {
+        if (root.left || root.right) {
+            return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+        }else {
+            return sum === root.val;
+        }
+    }
+    return false;
+};
