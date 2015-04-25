fe2a8d12dbf6c9b11685d6358f325c67a621206f
imcoddy <imcoddy@gmail.com>
2015-04-25 09:02:53 +0900

    imporve problem add-binary

--- add-binary.js
+++ add-binary.js
@@ -1,9 +1,48 @@
 /**
+ * Source:  https://leetcode.com/problems/add-binary/
+ * Tags:    [Math,String]
+ * Level:   Easy
+ * Title:   Add Binary
+ * Auther:  @imcoddy
+ * Content: Given two binary strings, return their sum (also a binary string).
+ *
+ *
+ *
+ * For example,
+ * a = "11"
+ * b = "1"
+ * Return "100".
+ */
+
+/**
  * @param {string} a
  * @param {string} b
  * @return {string}
+ *
  */
+
 var addBinary = function(a, b) {
+    var length = Math.max(a.length, b.length);
+    var result = '';
+    var up = 0;
+
+    for (var i = 0; i < length; i++) {
+        var d1 = a[a.length - i - 1] ? a[a.length - i - 1] : '0';
+        var d2 = b[b.length - i - 1] ? b[b.length - i - 1] : '0';
+        var value = parseInt(d1,10) + parseInt(d2, 10) + up;
+        result = '' + (value%2) + result;
+        up = value > 1 ? 1 : 0;
+    }
+    if (up) {
+        result = '1' + result;
+    }
+    return result;
+};
+
+/**
+ * Rumtime 245ms
+ */
+/*var addBinary = function(a, b) {
     var length = Math.max(a.length, b.length);
     var result = '';
     var up = '0'; // 0 for stay, 1 for up
@@ -29,7 +68,7 @@ var addBinary = function(a, b) {
         result = '1' + result;
     }
     return result;
-};
+};*/
 
 console.log(addBinary('0', '0')); // 0
 console.log(addBinary('0', '1')); // 1
