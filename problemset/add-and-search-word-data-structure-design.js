b08e185ad008695b5952f1f5207bd29c0af0c22b
imcoddy <imcoddy@gmail.com>
2015-05-21 13:37:54 +0900

    update problemset

---
+++ problemset/add-and-search-word-data-structure-design.js
@@ -0,0 +1,70 @@
+/**
+ * Source:  https://leetcode.com/problems/add-and-search-word-data-structure-design/
+ * Tags:    [Backtracking,Data Structure,Trie]
+ * Level:   Medium
+ * Title:   Add and Search Word - Data structure design
+ * Auther:  @imcoddy
+ * Content: Design a data structure that supports the following two operations:
+ *
+ *
+ * void addWord(word)
+ * bool search(word)
+ *
+ *
+ *
+ * search(word) can search a literal word or a regular expression string containing only letters a-z or .. A . means it can represent any one letter.
+ *
+ *
+ * For example:
+ *
+ * addWord("bad")
+ * addWord("dad")
+ * addWord("mad")
+ * search("pad") -> false
+ * search("bad") -> true
+ * search(".ad") -> true
+ * search("b..") -> true
+ *
+ *
+ *
+ * Note:
+ * You may assume that all words are consist of lowercase letters a-z.
+ *
+ *
+ * click to show hint.
+ *
+ * You should be familiar with how a Trie works. If not, please work on this problem: Implement Trie (Prefix Tree) first.
+ */
+
+/**
+ * @constructor
+ */
+var WordDictionary = function() {
+
+};
+
+/**
+ * @param {string} word
+ * @return {void}
+ * Adds a word into the data structure.
+ */
+WordDictionary.prototype.addWord = function(word) {
+
+};
+
+/**
+ * @param {string} word
+ * @return {boolean}
+ * Returns if the word is in the data structure. A word could
+ * contain the dot character '.' to represent any one letter.
+ */
+WordDictionary.prototype.search = function(word) {
+
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * var wordDictionary = new WordDictionary();
+ * wordDictionary.addWord("word");
+ * wordDictionary.search("pattern");
+ */
