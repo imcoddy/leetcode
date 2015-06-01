54a5714c3231389caa8de6cfaffd4cafc4d3d0a3
imcoddy <imcoddy@gmail.com>
2015-06-01 14:59:24 +0900

    update problemset

---
+++ problemset/contains-duplicate-ii.js
@@ -0,0 +1,17 @@
+/**
+ * Source:  https://leetcode.com/problems/contains-duplicate-ii/
+ * Tags:    [Array,Hash Table]
+ * Level:   Easy
+ * Title:   Contains Duplicate II
+ * Auther:  @imcoddy
+ * Content: Given an array of integers and an integer k, find out whether there there are two distinct indices i and j in the array such that nums[i] = nums[j] and the difference between i and j is at most k.
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+
+};
---
+++ problemset/contains-duplicate-iii.js
@@ -0,0 +1,18 @@
+/**
+ * Source:  https://leetcode.com/problems/contains-duplicate-iii/
+ * Tags:    [Binary Search Tree]
+ * Level:   Medium
+ * Title:   Contains Duplicate III
+ * Auther:  @imcoddy
+ * Content: Given an array of integers, find out whether there are two distinct indices i and j in the array such that the difference between nums[i] and nums[j] is at most t and the difference between i and j is at most k.
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @param {number} t
+ * @return {boolean}
+ */
+var containsNearbyAlmostDuplicate = function(nums, k, t) {
+
+};
