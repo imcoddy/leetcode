ba5e65c7f16df98aad378ae6ec61d3c4f4055bec
imcoddy <imcoddy@gmail.com>
2015-06-05 17:16:24 +0900

    Add util to convert between array and tree

--- util.js
+++ util.js
@@ -13,6 +13,61 @@ function TreeNode(val) {
     this.left = this.right = null;
 }
 
+util.arrayToTree = function(array) {
+    if (!array || array.length === 0 || array[0] === '#') return null;
+    var nodes = [];
+    for (var i = 0; i < array.length; i++) {
+        nodes[i] = array[i] === '#' ? null : new TreeNode(array[i]);
+    }
+    if (nodes.length === 1) return nodes[0];
+
+    var root = nodes[0];
+    var current = 0;
+    var next = 1;
+    var is_left = true;
+
+    while (next < nodes.length) {
+        if (nodes[current]) {
+            if (is_left) {
+                nodes[current].left = nodes[next++];
+            } else {
+                nodes[current].right = nodes[next++];
+                current++;
+            }
+            is_left = !is_left;
+        } else {
+            current++;
+        }
+    }
+
+    return root;
+};
+
+util.treeToArray = function(root) {
+    if (!root) return [];
+    if (!root.left && !root.right) return [root.val];
+
+    var result = [];
+    var queue = [root];
+    while (queue.length) {
+        var n = queue.shift();
+        if (n) {
+            result.push(n.val);
+            queue.push(n.left);
+            queue.push(n.right);
+        } else {
+            result.push('#');
+        }
+    }
+
+    while (result[result.length-1] === '#') {
+        result.pop();
+    }
+    return result;
+
+};
+
+
 util.arrayToLinkList = function(array) {
     if (!array) {
         return null;
